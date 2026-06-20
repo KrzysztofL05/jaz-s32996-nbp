fd3be80f25f55ec3c32dfe6a94250ba0ebc85e0c
Krzysztof <30lipiec03@gmail.com>
2026-06-20 14:05:54 +0200

    Drugi commit

---
+++ src/main/java/jaz_s32996_nbp/zaliczenie/config/AppConfig.java
@@ -0,0 +1,14 @@
+package jaz_s32996_nbp.zaliczenie.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
+
+@Configuration
+public class AppConfig {
+
+    @Bean
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
+}
