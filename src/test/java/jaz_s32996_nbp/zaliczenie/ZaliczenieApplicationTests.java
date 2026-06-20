d5f97862cd2c13ec4963c5a2bfe8a44885570871
Krzysztof <30lipiec03@gmail.com>
2026-06-20 12:34:53 +0200

    Pierwszy commit

---
+++ src/test/java/jaz_s32996_nbp/zaliczenie/ZaliczenieApplicationTests.java
@@ -0,0 +1,13 @@
+package jaz_s32996_nbp.zaliczenie;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZaliczenieApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
